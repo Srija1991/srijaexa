3cb6a9f7f2cc66c96f15c2cdc2245216fcb22793
Srija1991 <reachsrija@gmail.com>
2023-02-22 19:19:06 +0530

    Spring app

---
+++ src/main/java/com/example/srijaexa/SrijaexaApplication.java
@@ -0,0 +1,13 @@
+package com.example.srijaexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrijaexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrijaexaApplication.class, args);
+	}
+
+}
